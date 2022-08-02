75c8755a1928e79ce60add6daacf787d7e4f752d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-02 11:23:59 -0600

    update

---
+++ views/dashboard.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/mainStyle.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+
+
+    <title>CAMS | Home Page</title>
+</head>
+<body>
+    
+    <header>
+        <div class="logo">
+            <h1>
+                <a style="color:white; text-decoration:none;" href=""><i class="bi bi-file-earmark-check"></i> CAMS</a>
+            </h1>
+        </div>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li class="nav-list-item"><a class="nav-list-item-link" href="/">Home</a></li>
+                <li class="nav-list-item"><a class="nav-list-item-link" href="student-login.php">Students</a></li>
+                <li class="nav-list-item"><a class="nav-list-item-link" href="admin-login.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container text-end">
+        <br><br><br><br><br><br><br>
+        <div class="row">
+          <div class="col"></div>
+          <div class="col"></div>
+          <div class="col"></div>
+        </div>
+        <div class="row">
+          <div class="col"></div>
+          <div class="col">
+            <h1 style="color: white;">The Best Place to Track your <span style="color:#48ceff;">Assignments</span></h1>
+          </div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col">
+              <p style="color: white; font-weight: 100 !important;">Successful Careers start with Proactive Education</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col" style="margin-top: -23px;">
+                <span style="border-bottom: 3px solid #ffffff; color: rgba(0, 0, 0, 0);">color</span><span style="border-bottom: 3px solid #48ceff; margin-top: -150px !important; color: rgba(0, 0, 0, 0);">color</span>
+            </div>
+          </div>
+      </div>
+
+
+      <footer class="fixed-bottom py-3">
+        <div class="text-center text-muted" style="background-color: rgba(0, 0, 0, 0.05);">
+            &copy; 2022 Course Assignment Management System
+          </div>
+        </footer>
+
+
+</body>
+</html>
