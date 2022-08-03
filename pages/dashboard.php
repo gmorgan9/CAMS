6187e6c0b43ed8df08a83a4d6fdb61ca3c2514ab
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 14:04:24 -0600

    update

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -10,7 +10,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <!-- Custom Styles -->
-    <link rel="stylesheet" href="main-style.css">
+    <link rel="stylesheet" href="../main-style.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
@@ -23,12 +23,12 @@ session_start();
 </head>
 <body>
     
-<?php include("includes/header.php"); ?>
+<?php include("../includes/header.php"); ?>
 
-    dashboard
+    <h1>Dashboaed</h1>
 
 
-      <?php include("includes/footer.php"); ?>
+      <?php include("../includes/footer.php"); ?>
 
 
 </body>
--- student-registration.php
+++ student-registration.php
@@ -1,5 +1,11 @@
 <?php
 
+session_start();
+
+if(isset($_SESSION['username'])){
+   header('location:dashboard .php');
+}
+
 require_once "database/connection.php";
 
 if(isset($_POST['submit'])){
