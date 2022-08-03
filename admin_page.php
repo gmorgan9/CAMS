31d0c1966ad65af437d0194e9155d0881397ff5d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 13:33:28 -0600

    update

--- admin_page.php
+++ admin_page.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION['admin_fname']) && !isset($_SESSION['admin_lname'])){
 
    <div class="content">
       <h3>hi, <span>admin</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
+      <h1>welcome <span><?php echo $_SESSION['admin_fname'] ?></span></h1>
       <p>this is an admin page</p>
       <a href="login_form.php" class="btn">login</a>
       <a href="register_form.php" class="btn">register</a>
--- user_page.php
+++ user_page.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION['user_fname']) && !isset($_SESSION['user_lname'])){
 
    <div class="content">
       <h3>hi, <span>user</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['user_name'] ?></span></h1>
+      <h1>welcome <span><?php echo $_SESSION['user_fname'] ?></span></h1>
       <p>this is an user page</p>
       <a href="login_form.php" class="btn">login</a>
       <a href="register_form.php" class="btn">register</a>
