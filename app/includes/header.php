dc77bfffa056863dbee4fb9236ca781e8b012a8d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 16:02:23 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,10 +8,8 @@
             <ul class="nav-list">
                 
                 <?php if(isset($_SESSION['user_fname']) || isset($_SESSION['admin_fname'])){ ?>
-                    <li class="nav-list-item"><a class="nav-list-item-link" href="logout.php"><i class="bi bi-globe2"></i>  Dashboard</a></li>
+                    <li class="nav-list-item"><a class="nav-list-item-link" href="../dashboard.php"><i class="bi bi-globe2"></i>  Dashboard</a></li>
                     <li class="nav-list-item"><a class="nav-list-item-link" href="logout.php">Logout</a></li>
-
-
                 <?php } else { ?>
                     <li class="nav-list-item"><a class="nav-list-item-link" href="/">Home</a></li>
                     <li class="nav-list-item"><a class="nav-list-item-link" href="student-login.php">Login/Signup</a></li>
