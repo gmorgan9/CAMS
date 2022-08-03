1616e7604c04ac10c624be15c694c670a2abd451
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 15:02:05 -0600

    update

--- logout.php
+++ logout.php
@@ -12,7 +12,6 @@ require_once "app/database/connection.php";
 $uname = $_POST['uname'];
 $sql = "UPDATE students SET loggedin = '0' WHERE uname=$uname";
   mysqli_query($conn, $sql);
-  mysqli_close($conn);
 session_start();
 session_unset();
 session_destroy();
