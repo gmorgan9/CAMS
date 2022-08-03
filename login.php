08f76700ac5d03562c254b246e22a420e93cb21e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 16:39:52 -0600

    update

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 
-// require_once "app/database/connection.php";
+require_once "app/database/connection.php";
 require_once "app/database/functions.php";
 
 session_start();
@@ -17,57 +17,7 @@ if($loggedin === 1){
 
 
 
-if(isset($_POST['submit'])){
 
-   $sID = mysqli_real_escape_string($conn, $_POST['studentID']);
-   $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-   $lname = mysqli_real_escape_string($conn, $_POST['lname']);
-   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
-   $email = mysqli_real_escape_string($conn, $_POST['email']);
-   $pass = md5($_POST['password']);
-   $cpass = md5($_POST['cpassword']);
-   $isadmin = $_POST['isadmin'];
-   $loggedin = $_POST['loggedin'];
-
-   $select = " SELECT * FROM students WHERE uname = '$uname' && password = '$pass' ";
-
-   $result = mysqli_query($conn, $select);
-
-   if(mysqli_num_rows($result) > 0){
-
-      $row = mysqli_fetch_array($result);
-      $sql = "UPDATE students SET loggedin='1' WHERE uname='$uname'";
-      if($row['isadmin'] == 1){
-         if (mysqli_query($conn, $sql)) {
-            echo "Record updated successfully";
-          } else {
-            echo "Error updating record: " . mysqli_error($conn);
-          }
-         $_SESSION['admin_fname'] = $row['fname'];
-         $_SESSION['sID'] = $row['studentID'];
-         $_SESSION['loggedin'] = $row['loggedin'];
-         $_SESSION['admin_lname'] = $row['lname'];
-         $_SESSION['isadmin'] = $row['isadmin'];
-         header('location: /admin/profile.php');
-      }elseif($row['isadmin'] == 0){
-         if (mysqli_query($conn, $sql)) {
-            echo "Record updated successfully";
-          } else {
-            echo "Error updating record: " . mysqli_error($conn);
-          }
-         $_SESSION['user_fname'] = $row['fname'];
-         $_SESSION['sID'] = $row['sID'];
-         $_SESSION['loggedin'] = $row['loggedin'];
-         $_SESSION['user_lname'] = $row['lname'];
-         $_SESSION['isadmin'] = $row['isadmin'];
-         header('location: /profile.php');
-      }
-     
-   }else{
-      $error[] = 'incorrect email or password!';
-   }
-
-};
 ?>
 
 <!DOCTYPE html>
@@ -105,7 +55,7 @@ if(isset($_POST['submit'])){
       ?>
       <input type="text" name="uname" required placeholder="enter your user name">
       <input type="password" name="password" required placeholder="enter your password">
-      <input type="submit" name="submit" value="login now" class="form-btn">
+      <input type="submit" name="login_btn" value="login now" class="form-btn">
       <p>don't have an account? <a href="register.php">register now</a></p>
    </form>
 
