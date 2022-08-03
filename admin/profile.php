1f577795ad9d52e54649149024dbc2359ef535a3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 16:57:12 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -21,7 +21,7 @@ if(!isLoggedIn()){
    <title>admin page</title>
 
    <!-- Custom Styles -->
-   <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/main-style.css' ?>">
+   <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css' ?>">
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
