218df79e7907d8e753407b4c3e9d854c5966a810
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 14:59:03 -0600

    update

--- logout.php
+++ logout.php
@@ -2,13 +2,14 @@
 
 require_once "app/database/connection.php";
 
-$uname = $_SESSION['uname'];
-$sql = "UPDATE students SET loggedin='0' WHERE uname='$uname'";
-if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully";
-  } else {
-    echo "Error updating record: " . mysqli_error($conn);
-  }
+// $uname = $_SESSION['uname'];
+// $sql = "UPDATE students SET loggedin='0' WHERE uname='$uname'";
+// if (mysqli_query($conn, $sql)) {
+//     echo "Record updated successfully";
+//   } else {
+//     echo "Error updating record: " . mysqli_error($conn);
+//   }
+$_SESSION['loggedin'] = 0;
 session_start();
 session_unset();
 session_destroy();
