fb799a66c431320a73565986ac6e88e2fe26f07d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 14:47:29 -0600

    update

--- student-login.php
+++ student-login.php
@@ -31,13 +31,13 @@ if(isset($_POST['submit'])){
       $row = mysqli_fetch_array($result);
 
       if($row['isadmin'] == 1){
-         $_SESSION['loggedin'] = 1;
+         $_POST['loggedin'] = 1;
          $_SESSION['admin_fname'] = $row['fname'];
          $_SESSION['admin_lname'] = $row['lname'];
          header('location:admin_page.php');
 
       }elseif($row['isadmin'] == 0){
-         $_SESSION['loggedin'] = 1;
+         $_POST['loggedin'] = 1;
          $_SESSION['user_fname'] = $row['fname'];
          $_SESSION['user_lname'] = $row['lname'];
          header('location:user_page.php');
