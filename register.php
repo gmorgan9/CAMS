c93dcdb0b70b2aa233c544be1e84eb8fd8803fa9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 18:48:40 -0600

    update

--- register.php
+++ register.php
@@ -41,6 +41,51 @@ if(isLoggedIn()){
 // };
 
 
+// receive all input values from the form. Call the e() function
+    // defined below to escape form values
+    $fname    =  e($_POST['fname']);
+    $lname    =  e($_POST['lname']);
+    $uname    =  e($_POST['uname']);
+    $email    =  e($_POST['email']);
+    $pass     =  e($_POST['password']);
+    $cpass    =  e($_POST['cpassword']);
+ 
+    // form validation: ensure that the form is correctly filled
+    if (empty($fname)) { 
+       array_push($errors, "First name is required"); 
+    }
+    if (empty($lname)) { 
+       array_push($errors, "Last name is required"); 
+    }
+    if (empty($uname)) { 
+       array_push($errors, "Username is required"); 
+    }
+    if (empty($email)) { 
+       array_push($errors, "Email is required"); 
+    }
+    if (empty($pass)) { 
+       array_push($errors, "Password is required"); 
+    }
+    if ($pass != $cpass) {
+       array_push($errors, "The two passwords do not match");
+    }
+ 
+    // register user if there are no errors in the form
+    if (count($errors) == 0) {
+       $password = md5($pass);//encrypt the password before saving in the database
+          $query = "INSERT INTO students (fname, lname, uname, email, password, isadmin, loggedin) 
+                  VALUES('$fname', '$lname', '$uname', '$email', '$password', 0, 0)";
+          mysqli_query($conn, $query);
+ 
+          // get id of the created user
+          // $logged_in_user_id = mysqli_insert_id($conn);
+ 
+          // $_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
+          $_SESSION['success']  = "You are now logged in";
+          header('location: login.php');				
+    }
+
+
 ?>
 
 <!DOCTYPE html>
