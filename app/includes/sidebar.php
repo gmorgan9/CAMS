46ea9b2a1ba3682609c35580e58c25921f46afc7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 10:44:35 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -2,12 +2,12 @@
               <div class="position-sticky">
                 <div class="list-group list-group-flush mx-3 mt-4">
                   <a href="/" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                    <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>  Dashboard</span>
+                  <i class="bi bi-speedometer"></i><span>  Dashboard</span>
                   </a>
                   <a href="/pages/courses.php" class="list-group-item list-group-item-action py-2 ripple">
                   <i class="fa-solid fa-landmark"></i><span>  Courses</span></a>
-                  <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-clipboard-list"></i><span>  Assignments</span></a>
-                  <a href="/pages/progress-report.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-bars-progress"></i><span>  Progress Report</span></a>
+                  <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="bi bi-clipboard-data"></i><span>  Assignments</span></a>
+                  <a href="/pages/progress-report.php" class="list-group-item list-group-item-action py-2 ripple"><i class="bi bi-bar-chart"></i><span>  Progress Report</span></a>
                 </div>
               </div>
             </nav>
