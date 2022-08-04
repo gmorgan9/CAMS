5b682daedd52d870c887aefdff71a4868e420d3f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 12:38:40 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -47,7 +47,7 @@ if(!isLoggedIn()){
 
 
   <!-- stats -->
-<div class="stats d-flex justify-content-center" style="margin-left: 50px;">
+<div class="stats d-flex justify-content-center" style="margin-left: -50px;">
 
 
 
