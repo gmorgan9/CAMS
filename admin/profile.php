c567d7b7ae9b3ea48b22febdd635628ae2ad7bfe
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 14:36:58 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -83,11 +83,11 @@ if(!isLoggedIn()){
          <input class="form-control" id="fname" type="text" value="<?php echo $sID; ?>" name="fname" readonly>
       </div>
       <div class="row mx-auto">
-      <div class="form-group pt-3" style="width: 35%;">
+      <div class="form-group pt-3" style="width: 40%;">
          <label for="fname">First Name</label>
          <input class="form-control" id="fname" type="text" name="fname" required>
       </div>
-      <div class="form-group pt-3" style="width: 35%;">
+      <div class="form-group pt-3" style="width: 40%;">
          <label for="fname">Last Name</label>
          <input class="form-control" id="lname" type="text" name="lname" required>
       </div>
