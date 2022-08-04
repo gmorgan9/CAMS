c359029adb38f721f082ab72e3e62931a35e43aa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 09:44:02 -0600

    update

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require_once "app/database/functions.php";
 require_once "path.php";
 session_start();
 
-if(!isLoggedIn()){
-  header('location:/login.php');
+if(isLoggedIn()){
+  header('location:/dashboard.php');
 }
 
 ?>
