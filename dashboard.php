4af9c5c6f30f8b594bafefe2106e7428ceae1b94
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 13:13:59 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -27,9 +27,7 @@ if(!isLoggedIn()){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
     <style>
-      .detail-btn:hover {
-        color: #48ceff;
-      }
+      
     </style>
 
 
