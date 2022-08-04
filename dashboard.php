7104706dd12a122b96a62dd9a53441358b5def2e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 11:38:00 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -40,7 +40,7 @@ if(!isLoggedIn()){
 <div class="main">
   <div class="page-header" style="display:inline-block;">
     <p class="page_title" style="float: left;">Dashboard</p>
-    <nav aria-label="breadcrumb d-flex justify-content-end" style="float: right;">
+    <nav aria-label="breadcrumb pull-right" style="float: right;">
       <ol class="breadcrumb">
         <li class="breadcrumb-item"><a href="#">Home</a></li>
         <li class="breadcrumb-item active" aria-current="page">Library</li>
