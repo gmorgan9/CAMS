1667a2e12ffa1013ad4a7ffbc848b014ad24b0f8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 19:14:56 -0600

    update

--- profile.php
+++ profile.php
@@ -6,8 +6,8 @@ require_once "path.php";
 
 session_start();
 
-if(!isLoggedIn()){
-   header('location:../student-login.php');
+if(isLoggedIn() == false){
+   header('location: login.php');
 }
 
 ?>
@@ -36,7 +36,7 @@ if(!isLoggedIn()){
 
    <div class="content">
       <h3><span>Standard Profile Page</span></h3>
-      <h1>welcome <span><?php echo $_SESSION['admin_fname'] ?></span></h1>
+      <h1>welcome <span><?php echo $_SESSION['user_fname'] ?></span></h1>
       <p>this is an admin page</p>
       <a href="logout.php" class="btn">logout</a>
    </div>
