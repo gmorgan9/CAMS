fd8fff436d49f607b05e8741c4353c0a051484b6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 14:24:45 -0600

    udpate

--- admin/profile.php
+++ admin/profile.php
@@ -71,7 +71,7 @@ if(!isLoggedIn()){
 
 <div class="page-content mx-auto" style="margin-top: -6.5px;">
 <form action="" method="post">
-   <p class="subtitle text-muted text-center mx-auto" style="width: 95%;">Admin Profile Page</p>
+      <h3 class="mx-auto" style="width: 95%;">Admin Profile</h3>
       <?php
       if(isset($error)){
          foreach($error as $error){
