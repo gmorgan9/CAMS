64961ef0f7e0704e5fa4fb4fce8c9a1e9aa5a7d4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-03 18:51:42 -0600

    update

--- register.php
+++ register.php
@@ -7,85 +7,40 @@ if(isLoggedIn()){
    header('location: dashboard.php');
 }
 
-// if(isset($_POST['register'])){
-
-//    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
-//    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
-//    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
-//    $email = mysqli_real_escape_string($conn, $_POST['email']);
-//    $pass = md5($_POST['password']);
-//    $cpass = md5($_POST['cpassword']);
-//    $loggedin = 0;
-//    //$user_type = $_POST['user_type'];
-
-//    $select = " SELECT * FROM students WHERE uname = '$uname' && email = '$email' && password = '$pass' ";
-
-//    $result = mysqli_query($conn, $select);
-
-//    if(mysqli_num_rows($result) > 0){
-
-//       $error[] = 'user already exist!';
-
-//    }else{
-
-//       if($pass != $cpass){
-//          $error[] = 'Passwords do not match!';
-//       }else{
-//          $insert = "INSERT INTO students (fname, lname, uname, email, password) VALUES('$fname', '$lname', '$uname', '$email','$pass','$user_type')";
-//          mysqli_query($conn, $insert);
-//          echo "success";
-//          header('location:login.php');
-//       }
-//    }
-
-// };
-
-
 if(isset($_POST['register'])){
-// receive all input values from the form. Call the e() function
-    // defined below to escape form values
-    $fname    =  e($_POST['fname']);
-    $lname    =  e($_POST['lname']);
-    $uname    =  e($_POST['uname']);
-    $email    =  e($_POST['email']);
-    $pass     =  e($_POST['password']);
-    $cpass    =  e($_POST['cpassword']);
- 
-    // form validation: ensure that the form is correctly filled
-    if (empty($fname)) { 
-       array_push($errors, "First name is required"); 
-    }
-    if (empty($lname)) { 
-       array_push($errors, "Last name is required"); 
-    }
-    if (empty($uname)) { 
-       array_push($errors, "Username is required"); 
-    }
-    if (empty($email)) { 
-       array_push($errors, "Email is required"); 
-    }
-    if (empty($pass)) { 
-       array_push($errors, "Password is required"); 
-    }
-    if ($pass != $cpass) {
-       array_push($errors, "The two passwords do not match");
-    }
- 
-    // register user if there are no errors in the form
-    if (count($errors) == 0) {
-       $password = md5($pass);//encrypt the password before saving in the database
-          $query = "INSERT INTO students (fname, lname, uname, email, password, isadmin, loggedin) 
-                  VALUES('$fname', '$lname', '$uname', '$email', '$password', 0, 0)";
-          mysqli_query($conn, $query);
- 
-          // get id of the created user
-          // $logged_in_user_id = mysqli_insert_id($conn);
- 
-          // $_SESSION['user'] = getUserById($logged_in_user_id); // put logged in user in session
-          $_SESSION['success']  = "You are now logged in";
-          header('location: login.php');				
-    }
+
+   $fname = mysqli_real_escape_string($conn, $_POST['fname']);
+   $lname = mysqli_real_escape_string($conn, $_POST['lname']);
+   $uname = mysqli_real_escape_string($conn, $_POST['uname']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = md5($_POST['password']);
+   $cpass = md5($_POST['cpassword']);
+   $loggedin = 0;
+   //$user_type = $_POST['user_type'];
+
+   $select = " SELECT * FROM students WHERE uname = '$uname' && email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $error[] = 'user already exist!';
+
+   }else{
+
+      if($pass != $cpass){
+         $error[] = 'Passwords do not match!';
+      }else{
+         $insert = "INSERT INTO students (fname, lname, uname, email, password) VALUES('$fname', '$lname', '$uname', '$email','$pass','$user_type')";
+         mysqli_query($conn, $insert);
+         echo "success";
+         header('location:login.php');
+      }
    }
+
+};
+
+
 
 ?>
 
