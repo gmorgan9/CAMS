5b1c021c5d5776596e51c5818bd47f432f8c65fe
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 12:49:29 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -68,7 +68,7 @@ if(!isLoggedIn()){
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">View Details</p>
+    <p class="card-text text-end">View Details</p>
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div>
