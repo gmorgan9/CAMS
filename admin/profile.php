4052e38c35eb514ff5c0814171f37196b56f25f7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 16:11:55 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -36,9 +36,8 @@ if(isset($_POST['update-profile'])){
       // $error[] = 'user already exist!';
       $update = "UPDATE students SET fname = '$fname', lname = '$lname', uname = '$uname', email = '$email' where studentID = '$sID' ";
       mysqli_query($conn, $update);
-      // header('location:login.php');
-   }else{
-      
+      // header('location:login.php');}else{
+         $error[] = 'Success!';
    }
 };
 
