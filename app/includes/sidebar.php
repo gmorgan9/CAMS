ed4b6780feb197a277163def98a2513d9175a585
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 10:36:09 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,7 +1,7 @@
 <div class="left-sidebar">
     <ul>
-        <li><a href="#">Manage Posts</a></li>
-        <li><a href="#">Manage Users</a></li>
+        <li><a href="#">Courses</a></li>
+        <li><a href="#">Assignments</a></li>
         <li><a href="#">Manage Topics</a></li>
     </ul>
 </div>
