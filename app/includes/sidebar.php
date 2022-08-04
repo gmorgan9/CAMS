53cb2d69dffeffe114199913fc98ed73e4fb9838
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 12:17:57 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,19 +1,19 @@
-<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse">
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
     <div class="position-sticky">
         <div class="list-group list-group-flush mx-3 mt-4">
-            <a href="<?php echo BASE_URL . '/dashboard.php' ?>" class="list-group-item-dark list-group-item-action py-2 ripple" aria-current="true">
+            <a href="<?php echo BASE_URL . '/dashboard.php' ?>" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
                 <i class="bi bi-speedometer"></i>
                 <span>  Dashboard</span>
             </a>
-            <a href="<?php echo BASE_URL . '/pages/courses.php' ?>" class="list-group-item-dark list-group-item-action py-2 ripple">
+            <a href="<?php echo BASE_URL . '/pages/courses.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-mortarboard"></i>
                 <span>  Courses</span>
             </a>
-            <a href="<?php echo BASE_URL . '/pages/assignments.php' ?>" class="list-group-item-dark list-group-item-action py-2 ripple">
+            <a href="<?php echo BASE_URL . '/pages/assignments.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-clipboard-data"></i>
                 <span>  Assignments</span>
             </a>
-            <a href="<?php echo BASE_URL . '/pages/progress-report.php' ?>" class="list-group-item-dark list-group-item-action py-2 ripple">
+            <a href="<?php echo BASE_URL . '/pages/progress-report.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-bar-chart"></i>
                 <span>  Progress Report</span>
             </a>
