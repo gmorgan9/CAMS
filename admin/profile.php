7cc5286fa7517fb30c8fd9ea60384a2fdcb78fc0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 13:55:57 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -62,8 +62,8 @@ if(!isLoggedIn()){
     </ul>
   </div>
   <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
-  <div class="container">
-    <h3 class="display-6 text-center m-1">Welcome <span style="text-transform: capitalize;"><?php echo $_SESSION['admin_fname'] ?></span></h3>
+  <div class="container m-2">
+    <h3 class="display-6 text-center">Welcome <span style="text-transform: capitalize;"><?php echo $_SESSION['admin_fname'] ?></span></h3>
     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
   </div>
 </div>
