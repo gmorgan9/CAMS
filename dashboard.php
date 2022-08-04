457cb398ed9bae5cc09be2dbbefe867657be25f8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 12:46:15 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -47,14 +47,14 @@ if(!isLoggedIn()){
 
 
   <!-- stats -->
-<div class="stats d-flex justify-content-center" style="margin-left: -1px; margin-top: 5px !important; border-radius: 0 !important;">
+<div class="stats d-flex justify-content-center" style="margin-left: -1px; margin-top: 5px !important;">
 
 
 
 <div class="row">
 
 
-  <div class="card" style="width: 24rem; margin-right: 10px;">
+  <div class="card" style="width: 24rem; margin-right: 10px; border-radius: 0 !important;">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
