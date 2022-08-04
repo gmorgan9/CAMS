fb4748bf69e196e1fb046950dea896c23962a1d6
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 12:52:25 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -54,9 +54,9 @@ if(!isLoggedIn()){
 <div class="row">
 
 
-  <div class="card" style="width: 24.1rem; margin-right: 10px; border-radius: 0 !important; border-color: #fff;">
+  <div class="card" style="width: 24.1rem; margin-right: 20px; border-radius: 0 !important; border-color: #fff;">
   <div class="card-body">
-    <h5 class="card-title">Total Courses</h5>
+    <h5 class="card-title text-end">Total Courses</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text text-end">View Details</p>
     <a href="#" class="card-link">Card link</a>
@@ -66,7 +66,7 @@ if(!isLoggedIn()){
 
 <div class="card" style="width: 24.1rem; border-radius: 0 !important; border-color: #fff;">
   <div class="card-body">
-    <h5 class="card-title">Total Assignments Due</h5>
+    <h5 class="card-title text-end">Total Assignments Due</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
     <p class="card-text text-end">View Details</p>
     <a href="#" class="card-link">Card link</a>
