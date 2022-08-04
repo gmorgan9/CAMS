705c7dbb9510d6461e77aca0ff8cd300d7a90edd
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 14:26:33 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -64,7 +64,7 @@ if(!isLoggedIn()){
 
   <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
   <div class="container">
-    <h3 class="display-6 text-center" style="padding-top: 5px !important;">Welcome, <span style="text-transform: capitalize;"><?php echo $_SESSION['admin_fname'] ?>!</span></h3>
+    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php echo $_SESSION['admin_fname'] ?>!</span></h3>
   </div>
 </div>
 
