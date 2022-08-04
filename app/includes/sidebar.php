552ce78836ba160243df7ea08d0074b0d3f3b32b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 10:46:11 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -5,7 +5,7 @@
                   <i class="bi bi-speedometer"></i><span>  Dashboard</span>
                   </a>
                   <a href="/pages/courses.php" class="list-group-item list-group-item-action py-2 ripple">
-                  <i class="fa-solid fa-landmark"></i><span>  Courses</span></a>
+                  <i class="bi bi-mortarboard"></i><span>  Courses</span></a>
                   <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="bi bi-clipboard-data"></i><span>  Assignments</span></a>
                   <a href="/pages/progress-report.php" class="list-group-item list-group-item-action py-2 ripple"><i class="bi bi-bar-chart"></i><span>  Progress Report</span></a>
                 </div>
