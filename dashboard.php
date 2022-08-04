c37537fc8da443a5dce42ee5990bf01fc7e99a0b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 11:30:16 -0600

    update

--- dashboard.php
+++ dashboard.php
@@ -39,6 +39,12 @@ if(!isLoggedIn()){
         
 <div class="main">
     <h3 class="page_title">Dashboard</h3>
+    <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Library</li>
+  </ol>
+</nav>
 </div>
 
 
