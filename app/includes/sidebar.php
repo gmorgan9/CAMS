17536b70b46ed26b722717b4fd0f2d9a1bd2e83e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 10:38:34 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,7 +1,13 @@
-<div class="left-sidebar">
-    <ul>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Assignments</a></li>
-        <li><a href="#">Manage Topics</a></li>
-    </ul>
-</div>
+<nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+              <div class="position-sticky">
+                <div class="list-group list-group-flush mx-3 mt-4">
+                  <a href="/" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                    <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>  Dashboard</span>
+                  </a>
+                  <a href="/pages/courses.php" class="list-group-item list-group-item-action py-2 ripple">
+                  <i class="fa-solid fa-landmark"></i><span>  Courses</span></a>
+                  <a href="/pages/assignments.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-clipboard-list"></i><span>  Assignments</span></a>
+                  <a href="/pages/progress-report.php" class="list-group-item list-group-item-action py-2 ripple"><i class="fa-solid fa-bars-progress"></i><span>  Progress Report</span></a>
+                </div>
+              </div>
+            </nav>
