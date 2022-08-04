7503edf5438bce84ccd510d0b0888e46d01781ec
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 14:32:29 -0600

    update

--- admin/profile.php
+++ admin/profile.php
@@ -83,7 +83,7 @@ if(!isLoggedIn()){
          <input class="form-control" id="fname" type="text" value="<?php echo $sID; ?>" name="fname" readonly>
       </div>
       <div class="row">
-      <div class="form-group w-25">
+      <div class="form-group pt-3 w-25">
          <label for="fname">First Name</label>
          <input class="form-control" id="fname" type="text" name="fname" required>
       </div>
