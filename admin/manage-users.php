64ebefa2eab613090610087debfaf87d8179f2da
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 21:30:48 -0600

    update

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -108,6 +108,51 @@ if (mysqli_num_rows($result) > 0) {
 <!-- start PAGE-CONTENT -->
 <div class="page-content mx-auto">
 
+
+
+
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
       <?php 
       }
    } else {
