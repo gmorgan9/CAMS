c690d493b2d3f3dc211a9236fb4825e34ef74f31
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-04 21:17:06 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -21,6 +21,7 @@
             <?php if($_SESSION['isadmin'] == 1){ ?>
                 <br>
                 <span style="margin-left: 38px;">Admin Links</span>
+                <hr>
                 <a href="<?php echo BASE_URL . '/pages/progress-report.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-bar-chart"></i>
                 <span>  Manage Users</span>
