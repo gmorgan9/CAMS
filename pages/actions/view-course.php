91c24c6f9cc15ffb385242d3dbe8fc6cae0ce60f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 17:35:08 -0600

    update

--- pages/actions/view-course.php
+++ pages/actions/view-course.php
@@ -117,7 +117,7 @@ if(!isLoggedIn()){
                                     <h6 class="mb-0">Course</h6>
                                   </div>
                                   <div class="col-sm-9 text-secondary">
-                                        <?php echo $row['shortcourse']; ?> (00<?php echo $row['section']; ?>)
+                                        <?php echo $row['shortcourse']; ?> (00<?php echo $row['section']; ?>) <span class="text-black text-capitalize">Section <span class="text-muted"><?php echo $row['credits']; ?></span></span>
                                   </div>
                                 </div>
                       <hr>
