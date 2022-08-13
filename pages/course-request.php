ed81866ea2d0c2f833bad023ad75272950bb4678
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 11:32:41 -0600

    update

--- pages/course-request.php
+++ pages/course-request.php
@@ -40,7 +40,7 @@ if(!isLoggedIn()){
    {  
       $days .= $days1.",";  
    
-   $days=implode(", ", $_POST['days']);
+   //$days=implode(", ", $_POST['days']);
       // $insert2 = "INSERT INTO employee_company_data (employee_code, company_code, dept_code, job_code) SELECT employee_code, company_code, dept_code, jobID FROM job";
       $insert = "INSERT INTO course (idno, coursename, start_time, end_time, days, student_fname, student_lname, student_idno) VALUES('$idno', '$coursename', '$start_time', '$end_time', '$days', '$student_fname', '$student_lname', '$student_idno')";
     }  
