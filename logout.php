55bdc5cbad34077740f1f9e039e0db050bf9f8c7
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 07:26:12 -0600

    udpate

--- logout.php
+++ logout.php
@@ -4,7 +4,7 @@ session_start();
 require_once "app/database/connection.php";
 
 $sID = $_SESSION['sID'];
-$sql = "UPDATE students SET loggedin='0' WHERE studentID='$sID'";
+$sql = "UPDATE student SET loggedin='0' WHERE studentID='$sID'";
 if (mysqli_query($conn, $sql)) {
     echo "Record updated successfully";
 } else {
@@ -13,6 +13,6 @@ if (mysqli_query($conn, $sql)) {
 
 session_unset();
 session_destroy();
-header('location:login.php');
+header('location:' . BASE_URL . '/pages/entry/login.php');
 
 ?>
