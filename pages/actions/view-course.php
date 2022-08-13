20cbdab7e54d4bb60cd614321186b0cea09921a8
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 17:39:30 -0600

    update

--- pages/actions/view-course.php
+++ pages/actions/view-course.php
@@ -123,6 +123,15 @@ if(!isLoggedIn()){
                       <hr>
                                 <div class="row">
                                   <div class="col-sm-3">
+                                    <h6 class="mb-0">Professor</h6>
+                                  </div>
+                                  <div class="col-sm-9 text-secondary">
+                                        <?php echo $row['professorname']; ?>
+                                  </div>
+                                </div>
+                      <hr>
+                                <div class="row">
+                                  <div class="col-sm-3">
                                     <h6 class="mb-0">Credits</h6>
                                   </div>
                                   <div class="col-sm-9 text-secondary">
