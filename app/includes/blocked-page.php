6893671d66c6826b21b1e438d6d0870541cbb5c1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 10:35:15 -0600

    update

--- app/includes/blocked-page.php
+++ app/includes/blocked-page.php
@@ -1,6 +1,6 @@
 <br><br><br><br><br><br><br>
 <div class="text-center">
-    <span class="text-danger fs-3"><i class="bi bi-exclamation-octagon"></i> &nbsp; &nbsp; Blocked Page</span> <br>
+    <span class="text-danger fs-3"><i class="bi bi-exclamation-octagon"></i>&nbsp; Blocked Page</span> <br>
     <span class="text-black">This site is only a desktop only site!</span><br>
     <span class="text-black">Please visit this site with your laptop or computer!</span>
 </div>  
