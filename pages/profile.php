49e5d6a1b3ec5376373ac8fe408a4042981b05cf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 07:24:13 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -57,25 +57,13 @@ if(isset($_POST['update-profile'])){
    <!-- Custom Styles -->
    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-<!-- Bootstrap Icons -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+   <!-- Bootstrap Icons -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
 </head>
 <body>
-
-   
-<!-- <div class="land-container">
-   <div class="content">
-
-      <h3><span>Admin Profile Page</span></h3>
-      <h1>welcome <span><?php //echo $_SESSION['admin_fname'] ?></span></h1>
-      <p>this is an admin profile</p>
-      <a href="logout.php" class="btn">logout</a>
-   </div>
-
-</div> -->
 
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
@@ -94,7 +82,7 @@ if (mysqli_num_rows($result) > 0) {
   <div class="page-header mx-auto">
     <p class="page_title" style="float: left; padding-top: 2px;">Profile</p>
     <ul class="breadcrumb">
-      <li><a href="<?php echo BASE_URL . '/dashboard.php' ?>">Dashboard</a></li>
+      <li><a href="<?php echo BASE_URL . '/pages/dashboard.php' ?>">Dashboard</a></li>
       <li>Student Profile</li>
     </ul>
   </div>
@@ -117,7 +105,7 @@ if (mysqli_num_rows($result) > 0) {
          <div class="form-group pt-3" style="width: 20%;">
             <label for="status">Account Status</label>
             <?php
-            if($_SESSION['isadmin'] == 1){ 
+            if($_SESSION['acc_type'] == 1){ 
             ?>
             <input class="form-control" style="width: 90%" id="status" type="text" value="Admin" name="studentID" disabled>
             <?php 
@@ -138,8 +126,6 @@ if (mysqli_num_rows($result) > 0) {
             <label for="fname">Last Name</label>
             <input class="form-control" id="lname" type="text" name="lname" value="<?php echo $row['lname']; ?>" required>
          </div>
-      <!-- </div>end ROW -->
-      <!-- <div class="row" style="margin-left: 20px;"> -->
          <div class="form-group pt-3 mx-auto" style="width: 95%;">
             <label for="fname">User Name</label>
             <input class="form-control" id="uname" type="text" name="uname" value="<?php echo $row['uname']; ?>" required>
@@ -148,19 +134,6 @@ if (mysqli_num_rows($result) > 0) {
             <label for="fname">Email Address</label>
             <input class="form-control" id="email" type="email" name="email" value="<?php echo $row['email']; ?>" required>
          </div> 
-      <!-- </div> end ROW -->
-      <!-- <div class="row" style="margin-left: 20px;">
-         <div class="form-group pt-3" style="width: 48.6%;">
-            <label for="fname">Password</label>
-            <input class="form-control" id="password" type="password" name="password" value="<?php //echo $row['password']; ?>" required>
-            <i class="bi bi-eye-slash" id="togglePassword" style="cursor: pointer;"></i>  Show/Hide Password
-         </div>   
-         <div class="form-group pt-3" style="width: 48.6%;">
-            <label for="fname">Confirm Password</label>
-            <input class="form-control" id="cpassword" type="password" name="cpassword" value="<?php //echo $row['password']; ?>" required>
-         </div>
-      </div> -->
-      <!-- end ROW -->
 
       <div class="form-group pt-3 mx-auto" style="width: 95%; margin-bottom: 10px;">
       <input type="submit" name="update-profile" value="Update User" class="btn btn-secondary">
@@ -178,27 +151,7 @@ if (mysqli_num_rows($result) > 0) {
 </div> 
 
 
-<?php include(ROOT_PATH . "app/includes/footer.php"); ?>
-
-<!-- <script>
-   const togglePassword = document.querySelector("#togglePassword");
-        const password = document.querySelector("#password");
-
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            
-            // toggle the icon
-            this.classList.toggle("bi-eye");
-        });
-
-        // prevent form submit
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
-        });
-</script> -->
+<?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
 
 </body>
 </html>
