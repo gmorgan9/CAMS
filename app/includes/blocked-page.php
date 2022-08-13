238ad95f19057041d7252c0feeabb8a84fbf4e03
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 10:32:43 -0600

    update

--- app/includes/blocked-page.php
+++ app/includes/blocked-page.php
@@ -1,3 +1,5 @@
 <br><br><br>
-<span class="text-center text-black fs-3">Blocked Page</span> <br>
-<span class="text-center text-black">This site is only a desktop only site</span>
+<div class="text-center">
+    <span class="text-black fs-3">Blocked Page</span> <br>
+<   span class="text-black">This site is only a desktop only site</span>
+</div>  
