bd80712db2e48a8f1d8bba6c7319678fad49c9e0
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 11:33:46 -0600

    update

--- pages/course-request.php
+++ pages/course-request.php
@@ -34,16 +34,16 @@ if(!isLoggedIn()){
     $select = " SELECT * FROM course WHERE coursename = '$coursename' ";
     $result = mysqli_query($conn, $select);
 
-    $day1=$_POST['days'];  
-    $days=""; 
-    foreach($day1 as $days1)  
-   {  
-      $days .= $days1.",";  
-   
-   //$days=implode(", ", $_POST['days']);
+//     $day1=$_POST['days'];  
+//     $days=""; 
+//     foreach($day1 as $days1)  
+//    {  
+//       $days .= $days1.",";  
+   $var1 = $_POST['days'];
+   $days=implode(", ", $var1);
       // $insert2 = "INSERT INTO employee_company_data (employee_code, company_code, dept_code, job_code) SELECT employee_code, company_code, dept_code, jobID FROM job";
       $insert = "INSERT INTO course (idno, coursename, start_time, end_time, days, student_fname, student_lname, student_idno) VALUES('$idno', '$coursename', '$start_time', '$end_time', '$days', '$student_fname', '$student_lname', '$student_idno')";
-    }  
+   // }  
       mysqli_query($conn, $insert);
       // mysqli_query($conn, $insert2);
       header('location: course-request.php');
