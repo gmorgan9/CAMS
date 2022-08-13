4b35931793c501eeb160653326389490973472cf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-13 07:32:17 -0600

    update

--- pages/profile.php
+++ pages/profile.php
@@ -1,9 +1,9 @@
 <!-- WORKING -->
 <?php
 
-require_once "../../app/database/connection.php";
-require_once "../../app/database/functions.php";
-require_once "../../path.php";
+require_once "../app/database/connection.php";
+require_once "../app/database/functions.php";
+require_once "../path.php";
 
 session_start();
 
@@ -11,21 +11,16 @@ if(!isLoggedIn()){
    header('location:' . BASE_URL . '/pages/entry/login.php');
 }
 
-
 $sID = $_SESSION['sID'];
 $select = " SELECT * FROM student WHERE studentID = '$sID' ";
 $result = mysqli_query($conn, $select);
-
 if(isset($_POST['update-profile'])){
 
-   //$sID   = mysqli_real_escape_string($conn, $_POST['studentID']);
    $fname = mysqli_real_escape_string($conn, $_POST['fname']);
    $lname = mysqli_real_escape_string($conn, $_POST['lname']);
    $uname = mysqli_real_escape_string($conn, $_POST['uname']);
    $email = mysqli_real_escape_string($conn, $_POST['email']);
-   // $pass = md5($_POST['password']);
-   // $cpass = md5($_POST['cpassword']);
-   // $isadmin = $_POST['isadmin'];
+   $gender = mysqli_real_escape_string($conn, $_POST['gender']);
 
    $update_select = " SELECT * FROM student WHERE uname = '$uname' && email = '$email' ";
 
@@ -33,14 +28,10 @@ if(isset($_POST['update-profile'])){
 
    if(mysqli_num_rows($result) > 0){
 
-      // $error[] = 'user already exist!';
-      $update = "UPDATE student SET fname = '$fname', lname = '$lname', uname = '$uname', email = '$email' where studentID = '$sID' ";
+      $update = "UPDATE student SET fname = '$fname', lname = '$lname', uname = '$uname', email = '$email', gender = '$gender' where studentID = '$sID' ";
       mysqli_query($conn, $update);
       $success[] = 'Success';
-      header('location:' . BASE_URL . '/admin/profile.php');
-      
-   }else{
-      
+      header('Location: ' . $_SERVER['HTTP_REFERER']);
    } 
 };
 
@@ -57,26 +48,30 @@ if(isset($_POST['update-profile'])){
    <!-- Custom Styles -->
    <link rel="stylesheet" href="<?php echo BASE_URL . '/assets/css/other-style.css?v='. time(); ?>">
 
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
-   <!-- Bootstrap Icons -->
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+<!-- scripts -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </head>
 <body>
 
 <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
-
-
 <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
         
 <!-- start MAIN -->
 <div class="main"> 
-   
 <?php 
 
+$sID = $_SESSION['sID'];
+$select = " SELECT * FROM student WHERE studentID = '$sID' ";
+$result = mysqli_query($conn, $select);
 if (mysqli_num_rows($result) > 0) {
    while($row = mysqli_fetch_assoc($result)) {
+      $acc_type = $row['acc_type'];
 ?>
 
   <div class="page-header mx-auto">
@@ -87,57 +82,100 @@ if (mysqli_num_rows($result) > 0) {
     </ul>
   </div>
 
-  <div class="jumbotron jumbotron-fluid bg-white m-2 mx-auto" style="width: 98%;">
-  <div class="container">
-    <h3 class="display-6 text-center" style="padding-top: 5px !important;padding-bottom: 10px !important;">Welcome, <span style="text-transform: capitalize;"><?php echo $row['fname'] ?>!</span></h3>
-  </div>
-</div>
 
-<div class="page-content mx-auto">
+
+
+<div class="page-content mx-auto mt-2">
 <form action="" method="post">
-      <h3 class="mx-auto" style="width: 95%;">Student Profile</h3>
-      <div class="row" style="margin-left: 20px;">
-      <div class="form-group pt-3" style="width: 20%;">
-            <label for="studentID">Student ID</label>
-            <input class="form-control" style="width: 90%" id="studentID" type="text" value="<?php echo $row['studentID']; ?>" name="studentID" disabled>
-         </div>
+      <h3 class="mx-auto" style="width: 95%;">My Profile</h3>
 
-         <div class="form-group pt-3" style="width: 20%;">
-            <label for="status">Account Status</label>
-            <?php
-            if($_SESSION['acc_type'] == 1){ 
-            ?>
-            <input class="form-control" style="width: 90%" id="status" type="text" value="Admin" name="studentID" disabled>
-            <?php 
-            } else {
-            ?>
-            <input class="form-control" style="width: 90%" id="status" type="text" value="Student" name="studentID" disabled>
-            <?php 
-            }
-            ?>
-         </div>
-      </div>
-      <!-- <div class="row" style="margin-left: 20px;"> -->
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
-            <label for="fname">First Name</label>
-            <input class="form-control" id="fname" type="text" name="fname" value="<?php echo $row['fname']; ?>" required>
-         </div>
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
-            <label for="fname">Last Name</label>
-            <input class="form-control" id="lname" type="text" name="lname" value="<?php echo $row['lname']; ?>" required>
-         </div>
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
-            <label for="fname">User Name</label>
-            <input class="form-control" id="uname" type="text" name="uname" value="<?php echo $row['uname']; ?>" required>
-         </div>   
-         <div class="form-group pt-3 mx-auto" style="width: 95%;">
-            <label for="fname">Email Address</label>
-            <input class="form-control" id="email" type="email" name="email" value="<?php echo $row['email']; ?>" required>
-         </div> 
 
-      <div class="form-group pt-3 mx-auto" style="width: 95%; margin-bottom: 10px;">
-      <input type="submit" name="update-profile" value="Update User" class="btn btn-secondary">
-      <?php 
+      <div class="col-md-8 float-start w-25 ms-4">
+              <div class="card mb-3">
+                <div class="card-body">
+                  <img class="ms-1" src="../../assets/img/pic_holder.jpg" style="height: 250px; width: 250px; border-radius: 150px;" alt="">
+                  </div>
+                </div>
+              </div>
+
+
+
+      <div class="col-md-8 float-end me-4">
+              <div class="card mb-3">
+                <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Account Type</h6>
+                    </div>
+                    <div class="col-sm-9 text-info">
+                    <?php if($row['acc_type'] == 1) { ?>
+                        Admin
+                     <?php } if($row['acc_type'] == 0) { ?>
+                        Student
+                     <?php } ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Student</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="text-capitalize"><?php echo $row['lname']; ?>, <?php echo $row['fname']; ?> &nbsp; (<?php echo $row['idno']; ?>)</span>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Username</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                      <?php echo $row['uname']; ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Email <span class="text-muted" style="font-size: 10px;">Personal</span></h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                     <?php echo $row['email']; ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Gender</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <span class="text-capitalize"><?php echo $row['gender']; ?></span>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Student Status</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <?php if($row['status'] == 1) { ?>
+                        <span class="text-success">Active</span>
+                     <?php } if($row['status'] == 0) { ?>
+                        <span class="text-danger">Inactive</span>
+                     <?php } ?>
+                    </div>
+                  </div>
+                  <hr>
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <h6 class="mb-0">Actions</h6>
+                    </div>
+                    <div class="col-sm-9 text-secondary">
+                    <a style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#editStudent" class="badge text-bg-primary" href="#">Edit</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <?php 
       }
    } else {
      echo "0 results";
@@ -152,6 +190,95 @@ if (mysqli_num_rows($result) > 0) {
 
 
 <?php include(ROOT_PATH . "/app/includes/footer.php"); ?>
+
+
+<!-- Modal -->
+<div class="modal fade" id="editStudent" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit My Profile</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+<?php 
+//$id = $_GET['employeeID'];
+$select = " SELECT * FROM student WHERE studentID = '$sID' ";
+$result = mysqli_query($conn, $select);
+
+if (mysqli_num_rows($result) > 0) {
+   while($row = mysqli_fetch_assoc($result)) {
+    ?>
+
+<form action="" method="post">
+    <!-- <div class="section-header pt-2">
+      <span class="text-muted pt-4" style="width: 95%;"></span>
+    </div> -->
+    <!-- <hr style="margin-bottom: -5px; margin-top: 5px;"> -->
+    <?php 
+    $fname = $_SESSION['fname'];
+    $lname = $_SESSION['lname']; 
+    $employee_idno = $_SESSION['employee_idno'];?>
+      <!-- <input class="form-control" id="fname" type="text" name="fname" value="<?php //echo $fname; ?>"> -->
+      <!-- <input class="form-control" id="lname" type="text" name="lname" value="<?php //echo $lname; ?>"> -->
+      <!-- <input class="form-control" id="idno" type="text" name="idno" value="<?php //echo $idno; ?>"> -->
+    <div class="form-group pt-3" style="width: 35%;">
+      <label for="idno" style="font-size: 14px;">Student ID</label>
+      <input class="form-control" id="idno" type="text" name="idno" value="<?php echo $row['idno']; ?>" disabled>
+    </div>
+    <div class="row">
+      <div class="form-group pt-3" style="width: 50%;">
+        <label for="fname" style="font-size: 14px;">First Name</label>
+        <input class="form-control" id="fname" type="text" name="fname" value="<?php echo $row['fname']; ?>" required>
+      </div>
+      <div class="form-group pt-3" style="width: 50%;">
+        <label for="lname" style="font-size: 14px;">Last Name</label>
+        <input class="form-control" id="lname" type="text" name="lname" value="<?php echo $row['lname']; ?>" required>
+      </div>
+    </div>
+    <div class="row">
+      <div class="form-group pt-3" style="width: 50%;">
+        <label for="uname" style="font-size: 14px;">User Name</label>
+        <input class="form-control" id="uname" type="text" name="uname" value="<?php echo $row['uname']; ?>" required>
+      </div>
+      <div class="form-group pt-3" style="width: 50%;">
+        <label for="gender" style="font-size: 14px;">Gender</label>
+        <input class="form-control" id="gender" type="text" name="gender" value="<?php echo $row['gender']; ?>" required>
+      </div>
+    </div>
+    <div class="form-group pt-3">
+      <label for="email" style="font-size: 14px;">Email <span class="text-muted" style="font-size: 10px;">Personal</span></label>
+      <input class="form-control" id="email" type="email" name="email" value="<?php echo $row['email']; ?>" required>
+    </div>
+    <div class="form-group pt-3">
+      <label for="avatar" style="font-size: 14px;">Profile Picture</label>
+      <input class="form-control" id="avatar" type="file" name="avatar" value="<?php echo $row['avatar']; ?>">
+    </div>
+    
+    
+    
+    
+
+              <?php }} ?>
+
+
+
+
+
+
+      </div>
+      <div class="modal-footer">
+        <div class="form-group pt-3" style="margin-bottom: 10px;">
+          <button type="button" style="border-color: rgba(0,0,0,0);" class="badge text-bg-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" style="border-color: rgba(0,0,0,0);" name="update-profile" class="badge text-bg-secondary">Update Profile</button>
+        </div>
+      </div>
+      </form>
+    </div>
+  </div>
+  <!-- end modal -->
+</div>
 
 </body>
 </html>
