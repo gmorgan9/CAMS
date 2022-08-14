8a0a6f1ad66b90f76f636230b5d2d2c51b71d549
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 17:02:53 -0600

    update

--- pages/class-schedule.php
+++ pages/class-schedule.php
@@ -48,7 +48,7 @@ if(!isLoggedIn()){
 
 
     <!-- start PAGE-CONTENT -->
-<div class="page-content mx-auto mt-2" style="width: 100%;">
+<div class="page-content mx-auto mt-2" style="width: 105%;">
     <table class="table">
   <thead>
     <tr>
