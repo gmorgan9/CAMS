af280ddd8ac027d6f40463ed0e70a27bd563c36d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 17:29:04 -0600

    update

--- pages/class-schedule.php
+++ pages/class-schedule.php
@@ -146,7 +146,7 @@ if(isset($_GET['employeeID'])) {
       <span class="text-muted fs-3 pt-4" style="width: 95%;">Course Schedule for <?php echo $student_fname; ?> <?php echo $student_lname; ?></span>
     </div>
     <div class="section-header text-center pt-2">
-        <span class="text-muted fs-5 pt-1" style="width: 95%;"><?php echo $semester; ?></span>
+        <span class="text-muted fs-5" style="width: 95%;"><?php echo $semester; ?></span>
     </div>
     <table class="table">
   <thead>
