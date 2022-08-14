82b0a49b9d3f8ef75abd13dd1daa3b3f720c86d3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 17:01:24 -0600

    update

--- pages/class-schedule.php
+++ pages/class-schedule.php
@@ -38,12 +38,87 @@ if(!isLoggedIn()){
 <?php include(ROOT_PATH . "/app/includes/sidebar.php") ?>
         
 <div class="main">
+
   <div class="page-header mx-auto">
     <p class="page_title" style="float: left; padding-top: 2px;">Class Schedule</p>
     <ul class="breadcrumb">
       <li><a href="<?php echo BASE_URL . '/dashboard.php' ?>">Dashboard</a></li>
       <li>Class Schedule</li>
     </ul>
+
+
+    <!-- start PAGE-CONTENT -->
+<div class="page-content mx-auto mt-2">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID #</th>
+      <th scope="col">Employee</th>
+      <!-- <th scope="col">Company</th> -->
+      <!-- <th scope="col">Department</th> -->
+      <!-- <th scope="col">Postion</th> -->
+      <th scope="col">Status</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+
+  <?php
+      $student_idno = $_SESSION['student_idno'];
+      $sql = "SELECT * FROM course WHERE student_idno = '$student_idno'";
+      $all = mysqli_query($conn, $sql);
+      if($all) {
+          while ($row = mysqli_fetch_assoc($all)) {
+            $sID            = $row['studentID'];
+            $semester       = $row['semestername'];
+            $idno           = $row['idno'];
+            $shortcourse    = $row['shortcourse'];
+            $start_time     = $row['start_time'];
+            $end_time       = $row['end_time'];
+            $location       = $row['location'];
+            $student_fname  = $row['student_fname'];
+            $student_lname  = $row['student_lname'];
+            $student_idno   = $row['student_idno'];
+            $status         = $row['approval_status'];
+            ?>
+    <tr>
+        <th scope="row"><?php echo $idno; ?></th>
+        <td><?php echo $student_lname; ?>, <?php echo $student_fname; ?></td>
+        <!-- <?php //if($acc_type == 1){ ?>
+          <td>Admin</td>
+        <?php //} else { ?>
+          <td>Employee</td>
+        <?php //} ?> -->
+        <?php if($status == 1){ ?>
+          <td>Active</td>
+        <?php } else { ?>
+          <td>Inactive</td>
+        <?php } ?>
+        <td>
+          <!-- <a style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#editEmployee" class="badge text-bg-primary" href="actions/edit-employee.php?employeeID=<?php echo $empID; ?>">Edit</a> -->
+          <a style="text-decoration: none;" class="badge text-bg-success" href="actions/view-employee.php?employeeID=<?php echo $empID; ?>">View</a>
+          <a style="text-decoration: none;" data-bs-toggle="modal" data-bs-target="#confirmDelete" class="badge text-bg-danger" href="employees.php?employeeID=<?php echo $empID; ?>">Delete</a>
+        </td>
+        <?php }}?>
+  </tbody>
+</table>
+      <?php 
+  //     }
+  //  } else {
+  //    echo "0 results";
+  //  }
+      ?>
+ 
+ <!-- end PAGE-CONTENT -->
+</div>
+
+
+
+
+
+
+
+
   </div>
 </div>
 
