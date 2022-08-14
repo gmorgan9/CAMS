e9cbe39be99cdcdf0828e7d916d9bade9a7d5422
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 16:33:18 -0600

    update

--- admin/courses.php
+++ admin/courses.php
@@ -117,7 +117,7 @@ if(isset($_GET['courseID'])) {
 
 
 <!-- start PAGE-CONTENT -->
-<div class="page-content float-start" style="margin-top: 12px; width: 34%;margin-left: -65px; height: unset !important;">
+<div class="page-content float-start" style="margin-top: 12px; width: 34%;margin-left: -75px; height: unset !important;">
   <!-- <form action="" method="post"> -->
     <div class="section-header pt-2">
       <span class="text-muted pt-4" style="width: 95%;">Pending Course Requests</span>
