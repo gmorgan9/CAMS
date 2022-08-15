b3d5e0b1c8ae2b2fa7eda9c66d7a20725ac4f9eb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 18:30:50 -0600

    update

--- pages/assignments.php
+++ pages/assignments.php
@@ -64,14 +64,14 @@ if(!isLoggedIn()){
   <li class="nav-item" role="presentation">
     <button class="nav-link active" id="<?php echo $coursename; ?>" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true"><?php echo $coursename; ?></button>
   </li>
-  
+  <?php }} ?> 
 </ul>
 <div class="tab-content" id="myTabContent">
   <div class="tab-pane fade show active" id="<?php echo $coursename; ?>" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
     ...
   </div>
 </div>
-<?php }} ?> 
+
  
  <!-- end PAGE-CONTENT -->
 </div>
