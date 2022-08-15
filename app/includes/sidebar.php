d47364d0bdee814db541a7e4ad9f3d204bd59bc5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2022-08-14 18:08:02 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -14,7 +14,7 @@
                 <i class="bi bi-calendar-range"></i>
                 <span>  Schedule</span>
             </a>
-            <a href="<?php echo BASE_URL . '/pages/timesheet.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
+            <a href="<?php echo BASE_URL . '/pages/assignments.php' ?>" class="list-group-item list-group-item-action py-2 ripple">
                 <i class="bi bi-journal-bookmark"></i>
                 <span>  Assignments</span>
             </a>
